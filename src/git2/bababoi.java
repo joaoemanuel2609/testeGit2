a147a7e4e04a0ef98c533e4ed8d5c3bd25fffaf1
João Emanuel <joaoemanueldanielski@gmail.com>
2023-08-21 16:15:59 -0300

    via htṕs

---
+++ src/git2/bababoi.java
@@ -0,0 +1,11 @@
+package git2;
+
+public class bababoi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("via HTTPS");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author joao.danielski
+ *
+ */
+module git2 {
+}
